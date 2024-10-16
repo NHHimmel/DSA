a42bf6761e9dab317080d9309605db8c6f7b8c5c
ZENHIMEL <nazmul.hasan01705767@gmail.com>
2024-10-16 20:18:05 +0600

    deletion method had been added

--- binaryTree.py
+++ binaryTree.py
@@ -15,10 +15,6 @@ class BinaryTree:
          self.parent = node
       else:
          self.addHelper(self.parent, node)
-         #       2
-         #      / \
-         #      3 4
-         #     /\ /\
 
    @staticmethod
    def addHelper(head,node):
@@ -26,16 +22,24 @@ class BinaryTree:
       var.put(head)
       while not var.empty():
          curr = var.get()
-         if curr.left: var.put(curr.left)
-         else: curr.left = node; break
-         if curr.right: var.put(curr.right)
-         else: curr.right = node;break
+         
+         if curr.left: 
+            var.put(curr.left)
+         else: 
+            curr.left = node
+            break
 
+         if curr.right: 
+            var.put(curr.right)
+         else: 
+            curr.right = node
+            break
 
    def breadthSearch(self,key): 
       var = Queue()
       curr = self.parent
       if curr: var.put(curr)
+
       while not var.empty():
          curr = var.get()
          if curr.data == key: return True
@@ -43,12 +47,74 @@ class BinaryTree:
          if curr.right: var.put(curr.right)
       return False
 
+   def getDeepest(self):
+      var = Queue()
+      r_most = self.parent
+      if r_most: var.put(r_most)
+      while not(var.empty()):
+         r_most = var.get()
+         if r_most.left:
+            var.put(r_most.left)
+         if r_most.right:
+            var.put(r_most.right)  # Fix: use r_most.right instead of r_most.left
+      return r_most
+
+   def deleteDeepestRight(self, d_node):
+      var = Queue()
+      r_most = self.parent
+      if not r_most: return None
+      var.put(r_most)
+
+      while not var.empty():
+         r_most = var.get()
+         if r_most is d_node:
+            return None
+         if r_most.left:
+            if r_most.left == d_node:
+               r_most.left = None
+               return
+            else:
+               var.put(r_most.left)
+
+         if r_most.right:
+            if r_most.right == d_node:
+               r_most.right = None
+               return
+            else:
+               var.put(r_most.right)
+
+   def deleteNode(self, key):
+      var = Queue()
+      curr = self.parent
+      if not curr:
+         return
+      
+      if curr.data == key and not curr.left and not curr.right: 
+         self.parent = None
+         return
+
+      var.put(curr)
+
+      key_node = None
+      while not var.empty():
+         curr = var.get()
+         if curr.data == key:
+            key_node = curr  # Save the node to be deleted
+         if curr.left:
+            var.put(curr.left)
+         if curr.right:
+            var.put(curr.right)
+
+      if key_node:  # Replace the key_node's data with the deepest node's data
+         d_node = self.getDeepest()
+         key_node.data = d_node.data
+         self.deleteDeepestRight(d_node)
+
    def inOrderSearch(self,key):
       return self.inOrderHelper(self.parent,key)
 
    @staticmethod
    def inOrderHelper(head,key):
-      
       if head is None:
          return False
 
@@ -56,15 +122,14 @@ class BinaryTree:
       if result_left:
          return True
 
-      if head.data==key:
+      if head.data == key:
          return True
       
       return BinaryTree.inOrderHelper(head.right,key)
     
-
-   
-   def display(self): #pre Order traversal
-      self.displayHelper(self.parent) #or, displayHelper().__func__()
+   def display(self):  # Pre-order traversal
+      self.displayHelper(self.parent)
+      print()
 
    @staticmethod
    def displayHelper(node):
@@ -72,10 +137,8 @@ class BinaryTree:
          return
       else:
          print(node.data,end=" ")
-         BinaryTree.displayHelper(node.left) 
+         BinaryTree.displayHelper(node.left)
          BinaryTree.displayHelper(node.right)
-
-   
 
 if __name__ == "__main__":
    bt = BinaryTree()
@@ -85,13 +148,9 @@ if __name__ == "__main__":
    bt.add(10)
    bt.add(15)
 
-   bt.display()
+   bt.display()  # Pre-order: 2 5 7 10 15
+  
+   print("After deletion:")
+   bt.deleteNode(5)
 
-   print(bt.inOrderSearch(15))
-   
-
-
-
-
-      
-      
+   bt.display()  # Pre-order after deletion of node with value 5
